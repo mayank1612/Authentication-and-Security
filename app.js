d1d4bcdf9736dbce28359194056abd8a6509219b
mayank1612 <mayankbhagyawani101@gmail.com>
2021-06-17 23:01:07 +0530

    added o-auth20 authentication and implemented login via google

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const session = require('express-session');
 const passport=require('passport');
 const passportLocalMongoose=require('passport-local-mongoose');
 mongoose.set('useCreateIndex', true);
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+var findOrCreate = require('mongoose-findorcreate');
 
 const app=express();
 
@@ -31,25 +33,58 @@ mongoose.connect('mongodb://localhost:27017/userDB',
 
 const userSchema=new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User=mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      
+    User.findOrCreate({ googleId: profile.id,username: profile.emails[0].value }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
     res.render("home")
 })
 
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile","email"] }));
+  
+  app.get("/auth/google/secrets", 
+    passport.authenticate('google', { failureRedirect: "/login" }),
+    function(req, res) {
+      // Successful authentication, redirect secrects.
+      res.redirect("/secrets");
+    });
+
 app.get("/register",function(req,res){
     res.render("register")
 })
+
 
 app.get("/login",function(req,res){
     res.render("login")
