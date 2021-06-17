4ac9ea3d4bd2615ca8e289cfa662d32a59bd7492
mayank1612 <mayankbhagyawani101@gmail.com>
2021-06-17 12:04:45 +0530

    Added .gitignore and .env file with updated secret key

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
@@ -13,15 +14,13 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
-
 const userSchema=new mongoose.Schema({
     email:String,
     password:String
 });
 
-const secret="Thisismylittlesecret.";
 
-userSchema.plugin(encrypt, { secret:secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret:process.env.SECRET, encryptedFields: ['password'] });
 
 const User=mongoose.model("User",userSchema);
 
