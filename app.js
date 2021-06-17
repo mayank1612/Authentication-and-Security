43f294937708cdab3b869654d52b051da9f67ad4
mayank1612 <mayankbhagyawani101@gmail.com>
2021-06-17 11:42:34 +0530

    Level-2 Database encryption

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose = require('mongoose');
+const encrypt=require("mongoose-encryption");
 
 const app=express();
 
@@ -13,10 +14,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema={
+const userSchema=new mongoose.Schema({
     email:String,
     password:String
-}
+});
+
+const secret="Thisismylittlesecret.";
+
+userSchema.plugin(encrypt, { secret:secret, encryptedFields: ['password'] });
 
 const User=mongoose.model("User",userSchema);
 
